ee19c8659f03ca071f91a861b4e65bf143d8384c
Marcel Haberl <hey@marcel-haberl.at>
2024-02-11 00:56:03 +0100

    Improving the implementation of multilingualism

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,81 +1,57 @@
-let language = getSystemLang();
-
-const LANG = {
-    "de": {
-        "greeting_morning": "Guten Morgen",
-        "greeting_afternoon": "Schönen Tag",
-        "greeting_evening": "Schönen Abend",
-        "greeting_i_am": "ich bin Marcel Haberl.",
-        "description": "Ein gelernter Maschinenbautechniker und derzeit als Spengler bei einem der führenden Unternehmen Europas für Spezialfahrzeuge in der Bergland- und Kommunaltechnik tätig.",
-        "leisure": "In meiner Freizeit widme ich mich gerne der Hobby-Java-Softwarentwicklung. Darüber hinaus arbeite ich derzeit daran, meine Fähigkeiten im Bereich der Webentwicklung weiter auszubauen.",
-        "imprint": "Impressum & Datenschutz",
-        "cookie": "Diese Website verwendet Cookies, um Ihr Nutzererlebnis zu verbessern. Durch die Nutzung der Website stimmen Sie der Verwendung von Cookies gemäß der Datenschutzerklärung zu. ",
-        "cookie_btn": "Zustimmen und fortfahren"
-    },
-    "en": {
-        "greeting_morning": "Good morning",
-        "greeting_afternoon": "Good afternoon",
-        "greeting_evening": "Good evening",
-        "greeting_i_am": "I am Marcel Haberl.",
-        "description": "A trained mechanical engineering technician, currently working as a tinsmith at one of Europe's leading companies for special vehicles in mountain and municipal technology.",
-        "leisure": "In my leisure time, I take pleasure in pursuing hobby Java software development. Additionally, I am currently engaged in enhancing my skills in web development",
-        "imprint": "Imprint & Privacy",
-        "cookie": "This website uses cookies to enhance your user experience. By using the website, you consent to the use of cookies in accordance with the Privacy Policy.",
-        "cookie_btn": "Agree and Continue"
-    }
-};
+let currentLanguage = getSystemLang();
+let messages;
 
 document.addEventListener('DOMContentLoaded', function() {
     if (hasAcceptedCookies()) {
-        language = getCookie("lang");
+        currentLanguage = getCookie("lang");
     }
-    setMessages();
+    fetch("/assets/lang.json")
+        .then(res => res.json())
+        .then(json => messages = json)
+        .then(fetchExperiences)
+        .then(writeMessages)
 });
 
 function toggleLanguage() {
-    language = (language === 'de') ? 'en' : 'de';
-    setMessages();
+    currentLanguage = (currentLanguage === 'de') ? 'en' : 'de';
+    writeMessages()
     if (hasAcceptedCookies()) {
-        setCookie("lang", language);
+        setCookie("lang", currentLanguage);
     }
 }
 
-function setMessages() {
-    const MESSAGES = LANG[language];
-
-    writeMessage("greeting", getGreeting(MESSAGES));
-    writeMessage("greeting_i_am", MESSAGES.greeting_i_am);
-    writeMessage("description", MESSAGES.description);
-    writeMessage("leisure", MESSAGES.leisure);
-    writeMessage("imprint", MESSAGES.imprint);
-    writeMessage("cookie", MESSAGES.cookie);
-    writeMessage("cookie_btn", MESSAGES.cookie_btn);
+function getMessages() {
+    return messages[currentLanguage];
 }
 
-
-function writeMessage(id, message) {
-    const ELEMENT = document.getElementById(id);
-    if (ELEMENT) {
-        ELEMENT.innerText = message;
+function writeMessages() {
+    for (const ELEMENT of document.getElementsByClassName("greeting")) {
+        ELEMENT.innerText = getGreeting();
+    }
+    for (const key in getMessages()) {
+        if (key.startsWith("greeting_") && key !== "greeting_i_am") continue;
+        for (const ELEMENT of document.getElementsByClassName(key)) {
+            ELEMENT.innerText = getMessages()[key];
+        }
     }
 }
 
-function getGreeting(messages) {
+function getGreeting() {
     const TIME = new Date().getHours();
     if (TIME >= 0 && TIME < 11) {
-        return messages.greeting_morning;
+        return getMessages()["greeting_morning"];
     } else if (TIME >= 11 && TIME < 18) {
-        return messages.greeting_afternoon;
+        return getMessages()["greeting_afternoon"];
     } else {
-        return messages.greeting_evening;
+        return getMessages()["greeting_evening"];
     }
 }
 
 function getSystemLang() {
-    for (const lang of navigator.languages) {
-        if (lang.includes("en"))
+    for (const LANG of navigator.languages) {
+        if (LANG.includes("en"))
             return "en";
-        else if (lang.includes("de")) {
+        else if (LANG.includes("de")) {
             return "de";
         }
     }
