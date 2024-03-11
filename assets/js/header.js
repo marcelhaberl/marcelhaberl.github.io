d99a2d50f575a0d8a964cc264261052fd228d1b4
Marcel Haberl <hey@marcel-haberl.at>
2024-03-11 19:55:48 +0100

    The website is now responsive

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,20 +1,46 @@
 let lastY = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
-    window.addEventListener("scroll", () => {
-        const HEADER = document.querySelector("header");
-        if (!HEADER) return;
-
-        const CURRENT_Y = window.scrollY;
-        if (CURRENT_Y <= 0) return;
-
-        if (lastY >= CURRENT_Y) {
-            HEADER.style.top = "0";
-        } else {
-            if (!HEADER.matches(":hover")) {
-                HEADER.style.top = "-500px";
-            }
-        }
-        lastY = CURRENT_Y;
-    });
+    const HEADER = document.querySelector("header");
+    window.addEventListener("scroll", () => scroll(HEADER));
+    registerHeaderClickEvent(document.querySelector("#content header nav .menu"), HEADER);
+    for (const ELEMENT of HEADER.getElementsByTagName("a")) {
+        registerHeaderClickEvent(ELEMENT, HEADER);
+    }
 });
+
+function registerHeaderClickEvent(element, header) {
+    element.addEventListener("click", () => toggleMenu(header));
+}
+
+function scroll(header) {
+    if (!header) return;
+
+    const CURRENT_Y = window.scrollY;
+    if (CURRENT_Y <= 0) return;
+
+    if (lastY >= CURRENT_Y) {
+        header.style.top = "0";
+    } else {
+        if (!header.matches(":hover")) {
+            header.style.top = "-500px";
+        }
+    }
+    lastY = CURRENT_Y;
+}
+
+function toggleMenu(header) {
+    if (!header) return;
+
+    const MENU = header.querySelector("#content header nav .menu img.menu_item");
+    const CLOSE = header.querySelector("#content header nav .menu img.close_item");
+
+    MENU.classList.toggle("show");
+    CLOSE.classList.toggle("show");
+
+
+    const PARENT = document.querySelector("#content header nav");
+    for (const ELEMENT of PARENT.getElementsByTagName("a")) {
+        ELEMENT.classList.toggle("show");
+    }
+}
